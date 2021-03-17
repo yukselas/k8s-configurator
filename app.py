29ad0a85b9230211f7bf2f304c1cb7a657846037
Ayse Yuksel <aysesariyuksel@gmail.com>
2021-03-17 14:26:56 -0700

    initial

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from flask_bootstrap import Bootstrap
-import subprocess
+from shelljob import proc
+
 app = Flask(__name__)
 Bootstrap(app)
 app.config.from_mapping(
@@ -9,11 +10,24 @@ app.config.from_mapping(
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return 'Go to next step!'    
+        return 'Go to next step!'     
     return render_template('index.html')
 
-@app.route('/run')
-def terminal():
-    return
+
+@app.route( '/stream' )
+def stream():
+    g = proc.Group()
+    p = g.run( [ "bash", "test.sh" ] )
+
+    def read_process():
+        while g.is_pending():
+            lines = g.readlines()
+            for proc, line in lines:
+                yield "data: " +str(line) +"\n\n"
+
+    return Response( read_process(), mimetype= 'text/event-stream' )
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in $(seq 10); do
+	sleep 2
+	echo $i
+done
